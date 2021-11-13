0c9b60fb0b75829acaa590f70afb4d16e75be202
zhuhui <15901202236@163.com>
2021-11-13 19:14:21 +0800

    前端页面的第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/">首页</router-link>
+    <router-link to="/about">关于</router-link>
+    <router-view></router-view>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
+import router from './router'//导入自定义路由配置
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router//配置路由
 }).$mount('#app')
